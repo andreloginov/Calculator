c90806ad2524e80992aa4fa5133dcc9944e966d7
maksvaliullin <117544522+maksvaliullin@users.noreply.github.com>
2022-11-06 21:08:20 +0300

    Add project

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Main{
 
     public static void main(String[] args) throws Exception {
@@ -9,15 +8,14 @@ public class Main{
         calc(input);
     }
     public static void calc(String input) throws Exception {
-        System.out.println("Input:");
         Calculate newCalc = new Calculate();
-        System.out.println(newCalc.checkOperation1(input));
+        newCalc.checkOperation1(input);
         newCalc.splitInput(input);
 
         int romanSum = newCalc.checkNumberSystem(newCalc.getNewInput());
-        if (romanSum < 1) throw new Exception("т.к. в римской системе нет отрицательных чисел");
         try {
             if (newCalc.getNumberSystem().getName().equals("ROMAN")){
+                if (romanSum < 1) throw new Exception("т.к. в римской системе нет отрицательных чисел.");
                 StringBuilder translated = newCalc.split(romanSum);
                     System.out.println("Output:");
                     System.out.println(translated);
